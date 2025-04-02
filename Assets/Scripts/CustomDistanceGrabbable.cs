e963cf93edaaf0f067f8a2fb3799a1f80d6c0a78
surrealshimingliang <shiming.liang@surreal-interactive.com>
2025-04-02 15:49:50 +0800

    Add haptic when grabbing objects.

---
+++ Assets/Scripts/CustomDistanceGrabbable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomDistanceGrabbable : SVRDistanceGrabbable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void GrabBegin(SVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+        if (hand.gameObject.name.Contains("Left"))
+        {
+            SVRInput.TriggerHaptic(0, 1.0f, 50.0f, 0.2f);
+        }
+        else
+        {
+            SVRInput.TriggerHaptic(1, 1.0f, 50.0f, 0.2f);
+        }
+    }
+}
